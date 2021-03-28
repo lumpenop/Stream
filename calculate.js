ee6e4db7c9fbb6237e0a1b89d595a5f730a90a21
swk <swk@swkui-Macmini.local>
2021-03-28 21:51:25 +0900

    calculate 수정해야함

--- calculate.js
+++ calculate.js
@@ -1,8 +1,6 @@
 
-scoreBoard = [
-    0, 1, 3, 5, 7, 12, 11, 15, 20, 25,
-    30, 55, 40, 50, 60, 70, 135, 100, 150, 300
-]
+scoreBoard = [0, 1, 3, 5, 7, 12, 11, 15, 20, 25,
+    30, 55, 40, 50, 60, 70, 135, 100, 150, 300]
 
 color = ['blue','lightpurple','gray','black','green','red']
 colorCount = 0;
@@ -12,19 +10,24 @@ point = 0;
 score = () => {
     userList = document.querySelectorAll('.user_list > li');
     for (i=0; i < userList.length-1; i++){
+       
         beforeNum = parseInt(userList[i].innerHTML);
         afterNum = parseInt(userList[i+1].innerHTML);
-        if (i > 0 && userList[i].innerHTML == '☆'){
-            beforeNum = userList[i-1]
+        
+        if (i > 0 && userList[i-1].innerHTML == '☆'){
+            beforeNum = afterNum
         }
 
         if (beforeNum <= afterNum || userList[i+1].innerHTML == '☆'){
-            stream++;
+            stream = stream+1;
             userList[i].style.backgroundColor = color[colorCount];
             userList[i+1].style.backgroundColor = color[colorCount];
+
+          
         } else {
-            point += scoreBoard[stream];
+            point = point + scoreBoard[stream];
             stream = 0;
+
 
             if(colorCount < color.length){
                 colorCount++;
@@ -33,6 +36,7 @@ score = () => {
             }
         }
     }
+    point = point + scoreBoard[stream];
     document.querySelector('.scoring').innerHTML = point;
     return point;
 }
