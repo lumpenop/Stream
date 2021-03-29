9d5deacb5e9803ede8435971db6f70e6f23760b7
lumpenop <swk5373@gmail.com>
2021-03-29 17:12:19 +0900

    calculate 의 color 부분 수정해야 함

--- calculate.js
+++ calculate.js
@@ -5,7 +5,9 @@ scoreBoard = [0, 1, 3, 5, 7, 12, 11, 15, 20, 25,
 color = ['blue','lightpurple','gray','black','green','red']
 colorCount = 0;
 stream = 0;
-point = 0;
+total = 0;
+
+colorGroup = [];
 
 score = () => {
     userList = document.querySelectorAll('.user_list > li');
@@ -14,29 +16,44 @@ score = () => {
         beforeNum = parseInt(userList[i].innerHTML);
         afterNum = parseInt(userList[i+1].innerHTML);
         
-        if (i > 0 && userList[i-1].innerHTML == '☆'){
-            beforeNum = afterNum
+        if (i > 0 && userList[i].innerHTML == '☆'){
+            beforeNum = userList[i-1];
+        }
+        if (i > 0 && userList[i+1].innerHTML == '☆'){
+            afterNum = beforeNum;
         }
 
-        if (beforeNum <= afterNum || userList[i+1].innerHTML == '☆'){
-            stream = stream+1;
-            userList[i].style.backgroundColor = color[colorCount];
-            userList[i+1].style.backgroundColor = color[colorCount];
-
-          
-        } else {
-            point = point + scoreBoard[stream];
-            stream = 0;
-
-
-            if(colorCount < color.length){
-                colorCount++;
+        if (beforeNum <= afterNum){
+            if(stream == 0){
+                colorGroup.push(i);
+                colorGroup.push(i+1);
+                
             } else {
+                colorGroup.push(i+1);
+            }
+      
+            stream++;
+
+            if(i==userList.length-2){
+                total = total + scoreBoard[stream];
+            }
+      
+        } else {
+            total = total + scoreBoard[stream];
+            stream = 0;
+     
+            for(j=0; j < colorGroup.length; j++){
+                userList[colorGroup[j]].style.backgroundColor = color[colorCount];
+            }
+
+            colorCount++;
+            colorGroup = [];
+
+            if(colorCount == color.length){
                 colorCount = 0;
             }
-        }
     }
-    point = point + scoreBoard[stream];
-    document.querySelector('.scoring').innerHTML = point;
-    return point;
+    
+    document.querySelector('.scoring').innerHTML = total;
+    }
 }
